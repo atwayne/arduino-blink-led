5c5a16742102726c50db984210196671c1afe66f
Wayne Wang <nkgailei@gmail.com>
2021-07-03 19:59:42 +0800

    initial checkin

---
+++ src/blink-led/blink-led.ino
@@ -0,0 +1,11 @@
+#define LED 13  // The pin the LED is connected to
+void setup() {
+  pinMode(LED, OUTPUT); // Declare the LED as an output
+}
+
+void loop() {
+  digitalWrite(LED, HIGH); // Turn the LED on
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
